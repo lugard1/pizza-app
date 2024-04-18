661fd0a9063b46b486847c7ec7ae1430b943cfdf
Lugard Agu <agulugard@gmail.com>
2024-04-18 12:06:05 +0100

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact Component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
