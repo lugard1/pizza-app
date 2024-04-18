ecfa6518b591fc1196b3aee74bd6a84e84b256ae
Lugard Agu <agulugard@gmail.com>
2024-04-18 13:32:58 +0100

    Style Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact Component={Home} />
-        </Switch>
+        <Navbar className="App-logo" />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/menu' exact component={Menu} />
+          <Route path='/about' exact component={About} />
+          <Route path='/contact' exact component={Contact} />
+        </Routes>
       </Router>
     </div>
   );
