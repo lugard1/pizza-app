0bb9dedb92700c4e2c7bc26442db3d5ac5d02ea7
Lugard Agu <agulugard@gmail.com>
2024-04-21 10:43:57 +0100

    Created and styled contact page

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -1,9 +1,11 @@
 import Pepperoni from '../assets/pepperoni.jpg';
 import Margherita from '../assets/margherita.jpg';
-import TripleTechAcademy from '../assets/tripletechacademy.jpg';
+import TripleAcademy from '../assets/tripleacademy.jpg';
+import Vegan from '../assets/vegan.jpg';
+import Pineapple from '../assets/pineapple.jpg';
+import Expensive from '../assets/expensive.jpg'
 
-
-export const menuList = [
+export const MenuList = [
   {
     name: 'Pepperoni Pizza',
     image: Pepperoni,
@@ -15,23 +17,23 @@ export const menuList = [
     price: 1.11
   },
   {
-    name: 'TripleTechAcademy Pizza',
-    image: Pepperoni,
+    name: 'TripleAcademy Pizza',
+    image: TripleAcademy,
     price: 3.85
   },
   {
-    name: 'Pepper',
-    image: Pepperoni,
-    price: 1.58
+    name: 'Vegan Pizza',
+    image: Vegan,
+    price: 2.76
   },
   {
-    name: 'Pepper',
-    image: Pepperoni,
-    price: 1.58
+    name: 'Pineapple Pizza',
+    image: Pineapple,
+    price: 3.53
   },
   {
-    name: 'Pepper',
-    image: Pepperoni,
-    price: 1.58
+    name: 'Expensive Pizza',
+    image: Expensive,
+    price: 4.55
   }
 ]
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import PizzaLeft from '../assets/pizzaLeft.jpg'
+
+import '../styles/Contact.css';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide' style={{ backgroundImage: `url(${PizzaLeft})`}}></div>
+      <div className='rightSide'>
+        <h1>CONTACT US</h1>
+        <form id='contactForm' method='POST'>
+          <label htmlFor='name'>Full Name</label>
+          <input name='name' placeholder='Enter full name...' type='text'/>
+          <label htmlFor='email'>agulugard@gmail.com</label>
+          <input email='email' placeholder='Enter email...' type='email'/>
+          <label htmlFor='message'>Message</label>
+          <textarea
+          rows='6'
+            message='message'
+            placeholder='Enter message...'
+            type='Text'
+            required
+          ></textarea>
+          <button type='submit'>Send Message</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
